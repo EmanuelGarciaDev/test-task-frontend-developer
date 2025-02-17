1b29d241ffcdde45ed9487d819d3b0c1869f22da
Emanuel Dario Garcia <emanueldario.dev@gmail.com>
2025-02-17 20:22:19 +0100

    :active-pseudo class re-stylished

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <header>
               <div className="flex items-center justify-end gap-8 py-4">
                 <nav className="ml-auto text-sm font-medium space-x-6">
-                <a className="active:bg-slate-500 hover:rounded-md p-1 hover:bg-slate-500 focus:ring-2 focus:ring-bg-slate-500 focus:outline-none" href="/">HOME</a>
-                <a className="active:bg-slate-500 hover:rounded-md p-1 hover:bg-slate-500 focus:ring-2 focus:ring-bg-slate-500 focus:outline-none" href="/about">ABOUT</a>
+                <a className="active:bg-slate-500 hover:rounded-md p-1 hover:bg-slate-600 focus:ring-2" href="/">HOME</a>
+                <a className="active:bg-slate-500 hover:rounded-md p-1 hover:bg-slate-600 focus:ring-2" href="/about">ABOUT</a>
                 </nav>
                 <ModeToggle />
               </div>
